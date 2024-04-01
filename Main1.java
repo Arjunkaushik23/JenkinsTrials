0bfce0c3b62911c2264b3d5c46011d2c4858202c
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-04-01 23:52:17 +0530

    Initial commit

---
+++ Main1.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Arjun is here my boy");
+        System.out.println("This is the different type of person");
+        System.out.println(new Date());
+    }
+}
