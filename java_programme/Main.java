febd54cbcd93f266024d1d6485dfd973f53bb05c
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-06 16:39:22 +0530

    initial commit

---
+++ java_programme/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello from java is here");
+        System.out.println("This is the java frogramme which is want to print something");
+        System.out.println(new Date());
+        System.out.println("Hello from java is here");
+    }
+}
