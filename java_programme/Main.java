f1d8ebf2522a53299eaf2e7a553d6ba73c45237a
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-06 18:41:44 +0530

    new changes

--- java_programme/Main.java
+++ java_programme/Main.java
@@ -9,7 +9,6 @@ public class Main {
         System.out.println("Hello from java is here");
         System.out.println("This is the java frogramme which is want to print something");
         System.out.println(new Date());
-        System.out.println("Hello from java is here");
-        System.out.println("This is the java frogramme which is want to print something and this is the new change");
+        System.out.println("hello this is for testing jenkins app how we deploy and how we run that");
     }
 }
