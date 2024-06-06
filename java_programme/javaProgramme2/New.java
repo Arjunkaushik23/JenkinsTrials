e0621b6f8f29be3e58236bb53da6e71a0d544322
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-06 17:36:34 +0530

    third commit

---
+++ java_programme/javaProgramme2/New.java
@@ -0,0 +1,8 @@
+package javaProgramme2;
+
+public class New {
+    public static void main(String[] args) {
+        System.out.println("All tests passed in");
+        System.out.println("This is used for all the things");
+    }
+}
